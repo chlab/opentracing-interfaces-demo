30b8c2c1215ce27aa1cc4fb07308690c8a82c38a
Christof Leuenberger <christof.leuenberger@mobi.ch>
2019-06-18 14:05:20 +0200

    added extended version of span and more readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+## Issue
+
+Since opentracing's `Span.context()` has a return value of opentracing's `Span`, if you extend `SpanContext` with a custom implementation, you will get linting errors because TypeScript expects an opentracing `SpanContext`.
+
+If `Span.context()` had a return value of a `SpanContext` interface, this wouldn't be an issue as far as I can tell.
+
+## Reproduce
+
 Please run:
 
 ```
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import {
   Tracer as OpenTracer,
   SpanOptions,
-  Span,
+  Span as OpenSpan,
   SpanContext as OpenSpanContext
 } from "opentracing";
 
@@ -9,10 +9,25 @@ class SpanContext extends OpenSpanContext {
   spanId: string;
 }
 
+class Span extends OpenSpan {
+  __context: SpanContext;
+
+  context(): SpanContext {
+    return this.__context;
+  }
+
+  constructor() {
+    super();
+    this.__context = new SpanContext();
+  }
+}
+
 class Tracer extends OpenTracer {
   startSpan(operationName: string, options: SpanOptions = {}): Span {
     let parent: SpanContext;
 
+    // results in error:
+    // Property 'spanId' does not exist on type '() => SpanContext'
     parent = (options.childOf as Span).context.spanId;
 
     return new Span();
